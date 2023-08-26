00d559dab9b6ab2a67a38aa1d8df0d4548718d4d
Gigashad <141179928+Thom4x@users.noreply.github.com>
2023-08-25 19:41:22 -0500

    Update Ejercicio2.py

--- Clase5-Ago/Ejercicio2.py
+++ Clase5-Ago/Ejercicio2.py
@@ -1,1 +1,30 @@
+class Vehiculo:
 
+  marca: str
+  combustible: str
+
+  def __init__(self, marca, combustible):
+    self.marca = marca 
+    self.combustible = combustible
+
+  def encender(self):
+    pass
+
+  def arrancar(self):
+    pass
+
+  def __str__(self):
+    return "El vehiculo {} necesita gasolina {} para operar".format(self.marca, self.combustible)
+
+
+class Auto(Vehiculo):
+  pass
+
+class Moto(Vehiculo):
+    pass
+
+carra=Auto('Mazda', 'Corriente')
+print(carra)
+
+moto=Moto('Yamaha', 'Corriente')
+print(moto)
