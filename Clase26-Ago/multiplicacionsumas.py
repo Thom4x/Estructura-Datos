004eac6e46a360e9abe645de1088a29ccf667bce
Gigashad <141179928+Thom4x@users.noreply.github.com>
2023-08-25 19:48:46 -0500

    Update multiplicacionsumas.py

--- Clase26-Ago/multiplicacionsumas.py
+++ Clase26-Ago/multiplicacionsumas.py
@@ -1,1 +1,9 @@
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
 
+resultado_multiplicacion = 0
+
+for _ in range(numero2):
+    resultado_multiplicacion += numero1
+
+print(f"La multiplicación de {numero1} y {numero2} es: {resultado_multiplicacion}")
