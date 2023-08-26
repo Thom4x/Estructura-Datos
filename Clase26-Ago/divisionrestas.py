b579f0ce1379303d2a760d860a736fda5c9023d0
Gigashad <141179928+Thom4x@users.noreply.github.com>
2023-08-25 19:49:16 -0500

    Update divisionrestas.py

--- Clase26-Ago/divisionrestas.py
+++ Clase26-Ago/divisionrestas.py
@@ -1,1 +1,12 @@
+dividendo = int(input("Ingrese el dividendo: "))
+divisor = int(input("Ingrese el divisor: "))
 
+cociente = 0
+
+for _ in range(dividendo):
+    if dividendo >= divisor:
+        dividendo -= divisor
+        cociente += 1
+
+print(f"El cociente de la división es: {cociente}")
+print(f"El residuo de la división es: {dividendo}")
