594db47325a9e71afddc8d1809c72390ee462254
Gigashad <141179928+Thom4x@users.noreply.github.com>
2023-09-08 23:44:27 -0500

    Update EjercicioPilasCorregido.py

--- Clase2-Sep/EjercicioPilasCorregido.py
+++ Clase2-Sep/EjercicioPilasCorregido.py
@@ -1,30 +1,47 @@
+class Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+
 class Pila:
     def __init__(self):
-        self.items = []
+        self.cabeza = None
 
     def apilar(self, dato):
-        print(f"Agregando {dato} en la cima de la pila")
-        self.items.append(dato)
+        nuevo_nodo = Nodo(dato)
+        nuevo_nodo.siguiente = self.cabeza
+        self.cabeza = nuevo_nodo
 
     def imprimir(self):
-        print("Imprimiendo pila:")
-        print(self.items)
+        nodo_actual = self.cabeza
+        while nodo_actual is not None:
+            print(nodo_actual.dato)
+            nodo_actual = nodo_actual.siguiente
 
-    def elimnd(self, dato_a_eliminar):
-        try:
-            self.items.remove(dato_a_eliminar)
-        except ValueError:
-            print(f" Dato erroneo-no encontrado  {dato_a_eliminar} en la pila.")
+    def eliminar(self, dato_a_eliminar):
+        if self.cabeza is None:
+            return
+
+        if self.cabeza.dato == dato_a_eliminar:
+            self.cabeza = self.cabeza.siguiente
+            return
+
+        nodo_actual = self.cabeza
+        while nodo_actual.siguiente is not None and nodo_actual.siguiente.dato != dato_a_eliminar:
+            nodo_actual = nodo_actual.siguiente
+
+        if nodo_actual.siguiente is not None:
+            nodo_actual.siguiente = nodo_actual.siguiente.siguiente
 
 pila1 = Pila()
 for i in range(11):
     pila1.apilar(i)
 
-print("Pila original:")
+print("Pila original (de arriba hacia abajo):")
 pila1.imprimir()
 
-dato_a_eliminar = int(input("Ingresa el dato que desea eliminar : "))
-pila1.elimnd(dato_a_eliminar)
+dato_a_eliminar = int(input("Ingresa el dato que deseas eliminar: "))
+pila1.eliminar(dato_a_eliminar)
 
-print(f"Dato Eliminado {dato_a_eliminar}:")
+print("Pila después de eliminar el dato (de arriba hacia abajo):")
 pila1.imprimir()
