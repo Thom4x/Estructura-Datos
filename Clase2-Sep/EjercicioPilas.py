6e09ffc76cf6ecbb216748d558d77b5b01fe5519
Gigashad <141179928+Thom4x@users.noreply.github.com>
2023-09-15 15:25:41 -0500

    Update EjercicioPilas.py

--- Clase2-Sep/EjercicioPilas.py
+++ Clase2-Sep/EjercicioPilas.py
@@ -1,52 +1,58 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+class Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
 
-class ListaEnlazada:
-
+class Pila:
     def __init__(self):
-        self.cabeza = None
+        self.superior = None
 
-    def es_vacio(self):
-        return self.cabeza is None
-
-    def agregar_nodo(self, dato):
-      nodo = Node(dato)
-      if self.es_vacio():
-          self.cabeza = nodo
-      else:
-          nodo_actual = self.cabeza
-          while nodo_actual.next is not None:
-              nodo_actual = nodo_actual.next
-          nodo_actual.next = nodo
+    def apilar(self, dato):
+        print(f"Agregando {dato} en la cima de la pila")  
+        if self.superior == None:
+            self.superior = Nodo(dato)
+            return
+        nuevo_nodo = Nodo(dato)
+        nuevo_nodo.siguiente = self.superior
+        self.superior = nuevo_nodo
 
     def imprimir(self):
-      nodo_actual = self.cabeza
-      while nodo_actual is not None:
-          print(nodo_actual.data)
-          nodo_actual = nodo_actual.next
+        print("Imprimiendo pila:")
+        # Recorrer la pila e imprimir valores
+        nodo_temporal = (self.superior) 
+        while nodo_temporal != None:
+            print(f"{nodo_temporal.dato}", end=",")
+            nodo_temporal = nodo_temporal.siguiente
+        print("")
 
-    def eliminar(self, dato):
-      nodo_actual = self.cabeza
-      if nodo_actual.data == dato:
-          self.cabeza = nodo_actual.next
-          return
-      while nodo_actual.next is not None:
-          if nodo_actual.next.data == dato:
-              nodo_actual.next = nodo_actual.next.next
-              return
-          nodo_actual = nodo_actual.next
+    def eliminar_nodo(self, dato_a_eliminar):
+        if self.superior.dato == dato_a_eliminar:
+            self.superior = self.superior.siguiente
+            return
 
+        nodo_temporal = self.superior
+        while (nodo_temporal.siguiente is not None and nodo_temporal.siguiente.dato != dato_a_eliminar):
+            nodo_temporal = nodo_temporal.siguiente
 
-lista = ListaEnlazada()
-print("Agregamos datos al nodo")
-lista.agregar_nodo(1)
-lista.agregar_nodo(2)
-lista.agregar_nodo(3)
+        if nodo_temporal.siguiente is not None:
+            nodo_temporal.siguiente = nodo_temporal.siguiente.siguiente
+        else:
+            print(f"No se encontró el dato {dato_a_eliminar} en la pila.")
 
-print("Imprimimos los datos")
-lista.imprimir()
+pila1 = Pila()
+pila1.apilar(0)
+pila1.apilar(1)
+pila1.apilar(2)
+pila1.apilar(3)
 
+print("-"*50)
+print("Pila original:")
+pila1.imprimir()
+print("-"*50)
 
-print(lista.buscar(2))
+dato_a_eliminar = int(input("Ingrese el numero de la pila que desea eliminar dentro del rango de 0-10: "))  
+pila1.eliminar_nodo(dato_a_eliminar)
+
+print(f"Pila después de eliminar el dato {dato_a_eliminar}:")
+pila1.imprimir()
+print("-"*50)
