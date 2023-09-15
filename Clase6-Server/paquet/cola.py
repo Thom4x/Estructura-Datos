98571aa22ec4752e8de82ebbc52d718832d65197
Gigashad <141179928+Thom4x@users.noreply.github.com>
2023-09-15 17:43:30 -0500

    Update cola.py

--- Clase6-Server/paquet/cola.py
+++ Clase6-Server/paquet/cola.py
@@ -2,15 +2,16 @@ from .tipos import Item
 
 class Nodo:
     def __init__(self, valor: Item, mensaje_id: int):
-        self.valor = valor         
-        self.mensaje_id = mensaje_id  
-        self.siguiente = None       
+        self.valor = valor          # Valor del nodo, que es un objeto de la clase Item
+        self.mensaje_id = mensaje_id  # Identificador único del mensaje
+        self.siguiente = None        # Referencia al siguiente nodo en la cola
 
+# Definimos la clase Cola que representa una cola de mensajes
 class Cola:
     def __init__(self):
-        self.primero = None           
-        self.ultimo = None            
-        self.contador_id = 1          
+        self.primero = None           # Referencia al primer nodo en la cola
+        self.ultimo = None            # Referencia al último nodo en la cola
+        self.contador_id = 1          # Inicializamos un contador de IDs para los mensajes
 
     # Método para verificar si la cola está vacía
     def esta_vacia(self):
@@ -22,20 +23,20 @@ class Cola:
         if self.esta_vacia():
             self.primero = nuevo_nodo  # Si la cola está vacía, el nuevo nodo se convierte en el primero
         else:
-            self.ultimo.siguiente = nuevo_nodo      # Enlazamos el último nodo con el nuevo nodo
-        self.ultimo = nuevo_nodo                    # El nuevo nodo se convierte en el último
-        self.contador_id += 1                       # Se incrementa el contador de IDs para el siguiente mensaje
+            self.ultimo.siguiente = nuevo_nodo  # Enlazamos el último nodo con el nuevo nodo
+        self.ultimo = nuevo_nodo   # El nuevo nodo se convierte en el último
+        self.contador_id += 1     # Incrementamos el contador de IDs para el siguiente mensaje
 
     # Método para desencolar el primer mensaje de la cola
     def desencolar(self):
         if self.esta_vacia():
             return None
-        valor = self.primero.valor                   # Obtenemos el valor del primer mensaje
-        mensaje_id = self.primero.mensaje_id         # Obtenemos el ID del primer mensaje
-        self.primero = self.primero.siguiente        # El siguiente nodo se convierte en el primero
+        valor = self.primero.valor          # Obtenemos el valor del primer mensaje
+        mensaje_id = self.primero.mensaje_id  # Obtenemos el ID del primer mensaje
+        self.primero = self.primero.siguiente  # El siguiente nodo se convierte en el primero
         if self.primero is None:
             self.ultimo = None
-        return {"mensaje_id": mensaje_id, "valor": valor}  # Devolvemos el mensaje desencolado como un diccionario
+        return {"mensaje_id": mensaje_id, "valor": valor}  # Devolvemos el mensaje desencolado como un texto
 
     # Método para ver todos los mensajes en la cola
     def ver_listado(self):
@@ -67,6 +68,25 @@ class Cola:
         while actual:
             count += 1
             actual = actual.siguiente
-        return count # El mensaje o solicitud se eliminó exitosamente
+        return count
+
+    # Método para eliminar un mensaje específico de la cola por su mensaje_id
+    def eliminar_mensaje(self, mensaje_id: int):
+        actual = self.primero
+        anterior = None
+
+        while actual and actual.mensaje_id != mensaje_id:
+            anterior = actual
+            actual = actual.siguiente
+
+        if actual:
+            if anterior:
+                anterior.siguiente = actual.siguiente
+            else:
+                self.primero = actual.siguiente
+
+            if not actual.siguiente:
+                self.ultimo = anterior
+            return True  # El mensaje se eliminó exitosamente
 
         return False  # El mensaje con el mensaje_id no se encontró en la cola
