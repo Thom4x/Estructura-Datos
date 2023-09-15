c803eac1575131511a6dc446c3dc3199af406027
Gigashad <141179928+Thom4x@users.noreply.github.com>
2023-09-15 15:23:04 -0500

    Update NodosRecursion.py

--- Clase26-Ago/NodosRecursion.py
+++ Clase26-Ago/NodosRecursion.py
@@ -3,26 +3,30 @@ import random
 class Nodo:
     def __init__(self, dato):
         self.dato = dato
-        self.siguiente = None
+        self.direccion = None
 
-def crear_lista_enlazada(cantidad_nodos):
-    primer_nodo = None
-    for _ in range(cantidad_nodos):
-        dato = random.randint(1, 100)
-        nuevo_nodo = Nodo(dato)
-        nuevo_nodo.siguiente = primer_nodo
-        primer_nodo = nuevo_nodo
-    return primer_nodo
+def crear_nodo_aleatorio(n):
+    if n <= 0:
+        return None
+    else:
+        dato_aleatorio = random.randint(1,50)  
+        nodo = Nodo(dato_aleatorio)
+        nodo.direccion = crear_nodo_aleatorio(n - 1)
+        return nodo
 
-def mostrar_lista_enlazada(nodo):
-    actual = nodo
-    i = 1
-    while actual:
-        print("Nodo", i, ":", actual.dato)
-        actual = actual.siguiente
-        i += 1
+def mostrar_nodos(nodo, numero_nodo=1):
+    if nodo is None:
+        return []
+    else:
+        resultado_actual = f"Nodo {numero_nodo}: {nodo.dato}"
+        return [resultado_actual] + mostrar_nodos(nodo.direccion, numero_nodo + 1)
 
-cantidad_nodos = int(input("Ingresa la cantidad de nodos: "))
-nodo_inicial = crear_lista_enlazada(cantidad_nodos)
-print("Lista enlazada generada:")
-mostrar_lista_enlazada(nodo_inicial)
+num_nodos = int(input("Ingrese la cantidad de nodos a crear: "))
+print("-"*45)
+print(f"La cantidad de nodos creados son: {num_nodos}")
+nodo_inicial = crear_nodo_aleatorio(num_nodos)
+datos_nodos = mostrar_nodos(nodo_inicial)
+
+for dato in datos_nodos:
+    print(dato)
+print("-"*45)
