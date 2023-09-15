ff7a26d1869ca9352acb09de6f1e9b6f5fdf4320
Gigashad <141179928+Thom4x@users.noreply.github.com>
2023-09-15 15:19:37 -0500

    Update DivRestas.py

--- Clase19-Ago/DivRestas.py
+++ Clase19-Ago/DivRestas.py
@@ -1,1 +1,14 @@
+def diviporrestas(dividendo, divisor):
+    cociente = 0
+    for _ in range(int(dividendo // divisor)):
+        dividendo -= divisor
+        cociente += 1
+    return cociente, dividendo
 
+dividendo = float(input("Ingresa el dividendo: "))
+divisor = float(input("Ingresa el diviso: "))
+
+cociente, residuo = diviporrestas(dividendo, divisor)
+print(f"La división de {dividendo} / {divisor} usando restas es:")
+print(f"Cociente: {cociente}")
+print(f"Residuo: {residuo}")
